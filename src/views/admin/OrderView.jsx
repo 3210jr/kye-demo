e7624d51c96b40b06dffe409887d7480fe676576
ghmeec <georgemillanzi1234@gmai.com>
2019-10-12 22:11:33 +0300

    initial commit in OrderView screentypes UI
    This commits include added ui components in with no logic

--- src/views/admin/OrderView.jsx
+++ src/views/admin/OrderView.jsx
@@ -3,7 +3,7 @@ import React, { Component, useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { upperFirst, clone } from "lodash";
-import { Paper, Typography, Button, TextField, MenuItem } from "@material-ui/core";
+import { Paper, Typography, Button, TextField, MenuItem,Grid,Select,FormControl,InputLabel } from "@material-ui/core";
 import { acceptOrder, rejectOrder, requestOrderChanges, completeOrder } from "../../utils";
 
 // const useStyles = makeStyles(theme => ({
@@ -147,30 +147,548 @@ class OrderView extends Component {
 
 				{/* {order.screeningTypes.map(type => ( */}
 				{order.screeningTypes.includes("identification") && <IdentityCheck order={order} />}
-				{order.screeningTypes.includes("police-reports") && <PoliceReports order={order} />}
 				{order.screeningTypes.includes("employment-history") && (
 					<EmploymentHistory order={order} />
 				)}
+				
+				{/* {order.screeningTypes.includes("police-reports") && <PoliceReports order={order} />} */}
+
+				
+
+				{/* always visible till when ur complete */}
+				{/* for police reports and academic reports */}
+				{true && <AcademicReports order={order} />}
+				{true && <PoliceReports order={order} />}
+				{true && <GapsReports order={order} />}
+				
 				{/* ))} */}
 			</div>
 		);
 	}
 }
 
-function PoliceReports({ order }) {
+function AcademicReports({ order }) {
 	return (
 		<Paper style={{ padding: "1em", marginTop: 15 }}>
-			<Typography variant="h6">Police Reports</Typography>
-			(in development)
+			<Typography variant="h6">Academic Qualification</Typography>
+			
+			{/* first row  */}
+			<Grid container spacing={3} style={{marginTop:5}}>
+					<Grid item xs={3} style={{paddingLeft:3,paddingRight:3}}>
+						<TextField
+							id="outlined-name"
+							label="Establish Name"
+							fullWidth
+							onChange={()=>{
+							}}
+							margin="normal"
+							variant="outlined"
+							style={{ margin: 3 }}
+						/>	
+					</Grid>
+
+					<Grid item xs={3} style={{paddingLeft:3,paddingRight:3}}>
+						<TextField
+							id="outlined-name"
+							label="Reference Method"
+							fullWidth
+							onChange={()=>{
+							}}
+							margin="normal"
+							variant="outlined"
+							style={{ margin: 3 }}
+						/>	
+					</Grid>
+
+					<Grid item xs={3} style={{paddingLeft:3,paddingRight:3}}>
+						<TextField
+							id="outlined-name"
+							label="Date Supplied"
+							fullWidth
+							onChange={()=>{
+							}}
+							margin="normal"
+							variant="outlined"
+							style={{ margin: 3 }}
+						/>	
+					</Grid>
+
+					<Grid item xs={3} style={{paddingLeft:3,paddingRight:3}}>
+						<FormControl 
+							fullWidth
+							variant="outlined"
+						>
+							<InputLabel htmlFor="select-edu">Did candidate study at this Establishment? </InputLabel>
+							<Select
+							variant="outlined"
+							onChange={()=>{
+
+							}}
+							inputProps={{
+								name: 'select-edu',
+								id: 'select-edu',
+							}}
+							>
+								<MenuItem value="">To be filled</MenuItem>
+								<MenuItem value="">Other</MenuItem>
+							</Select>
+						</FormControl>
+					</Grid>
+				
+			
+			</Grid>
+			{/* first row ends */}
+
+			{/* education details headers */}
+			<Grid container spacing={3} style={{marginTop:10}}>
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<Typography variant="h6">Candidate</Typography>
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<Typography variant="h6">Reference</Typography>
+				</Grid>	
+			
+			</Grid>
+
+			{/* education details headers ends */}
+
+			{/* education details headers contents */}
+			<Grid container spacing={3} style={{marginTop:10}}>
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+				<p>Attendance Date</p>					
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>
+				</Grid>	
+			
+			</Grid>
+
+
+			<Grid container spacing={3} style={{marginTop:10}}>
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+				<p>Name of course(s) studied</p>					
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>
+				</Grid>	
+			
+			</Grid>
+
+
+			<Grid container spacing={3} style={{marginTop:10}}>
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+				<p>Qualificication and grade awarded</p>					
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>
+				</Grid>	
+			
+			</Grid>
+
+
+			<Grid container spacing={3} style={{marginTop:10}}>
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+				<p>Qualification check - System score</p>					
+				</Grid>
+
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+					<Select
+
+						variant="outlined"
+						onChange={()=>{
+						}}
+						inputProps={{
+							name: 'edu-score',
+							id: 'edu-score',
+						}}
+						fullWidth
+						>
+							<MenuItem value="">Good</MenuItem>
+							<MenuItem value="">Normal</MenuItem>
+							<MenuItem value="">Bad</MenuItem>
+						</Select>
+				
+				</Grid>	
+				
+				<Grid item xs={4} style={{paddingLeft:3,paddingRight:3}}>
+		
+				</Grid>
+
+			
+			</Grid>
+
+			{/* education details headers ends */}
+
+			
 		</Paper>
 	);
 }
 
+
+function PoliceReports({ order }) {
+	return (
+		<Paper style={{ padding: "1em", marginTop: 15 }}>
+			<Typography variant="h6">Police Reports</Typography>
+			
+		</Paper>
+	);
+}
+
+function GapsReports({ order }) {
+	return (
+		<Paper style={{ padding: "1em", marginTop: 15 }}>
+			<Typography variant="h6">Gap Analysis</Typography>
+			
+		</Paper>
+	);
+}
+
+
+
+
+
+// 	this codes to be moved to separate js file
+//	EmploymentHistory component
 function EmploymentHistory({ order }) {
 	return (
 		<Paper style={{ padding: "1em", marginTop: 15 }}>
-			<Typography variant="h6">Employment History</Typography>
-			(in development)
+			<Typography variant="h6">Employment History and References</Typography>
+
+			{/* first row */}
+
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}> 
+				<TextField
+					id="outlined-name"
+					label="Organization"
+					fullWidth
+					onChange={()=>{
+
+					}}
+					margin="normal"
+					variant="outlined"
+					style={{ margin: 3 }}
+				/>
+				</Grid>
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						label="Reference method"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>	
+					
+				</Grid>
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						label="Date Produced"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>	
+					
+
+				</Grid>
+			</Grid>
+
+		
+			{/* second row */}
+
+			<p style={{paddingLeft:6}}>Position held</p>
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}> 
+				<TextField
+					id="outlined-name"
+					label="Candidate"
+					fullWidth
+					onChange={()=>{
+
+					}}
+					margin="normal"
+					variant="outlined"
+					style={{ margin: 3 }}
+				/>
+				</Grid>
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						id="outlined-name"
+						label="Referee"
+						fullWidth
+						onChange={()=>{
+
+						}}
+						margin="normal"
+						variant="outlined"
+						style={{ margin: 3 }}
+					/>	
+					
+
+				</Grid>
+				
+
+				
+			</Grid>
+			{/* second row ends */}
+
+			{/* third row */}
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}> 
+					<Grid item xs={4}>
+						<p style={{paddingLeft:6}}>Employment Start Date</p>
+					</Grid>
+					<Grid container>
+						<Grid item xs={6} style={{paddingLeft:3,paddingRight:3}}>
+							<TextField
+								id="outlined-name"
+								label="Candidate"
+								fullWidth
+								onChange={()=>{
+
+								}}
+								margin="normal"
+								variant="outlined"
+								style={{ margin: 3 }}
+							/>	
+
+						</Grid>
+						
+						<Grid item xs={6} style={{paddingLeft:3,paddingRight:3}}>
+							<TextField
+								id="outlined-name"
+								label="Referee"
+								fullWidth
+								onChange={()=>{
+
+								}}
+								margin="normal"
+								variant="outlined"
+								style={{ margin: 3 }}
+							/>	
+
+						</Grid>
+					</Grid>
+				</Grid>
+
+
+				<Grid item xs style={{paddingLeft:3,paddingRight:3}}> 
+					<Grid item xs={4}>
+						<p style={{paddingLeft:6}}>Employment End Date</p>
+					</Grid>
+					<Grid container>
+						<Grid item xs={6} style={{paddingLeft:3,paddingRight:3}}>
+							<TextField
+								id="outlined-name"
+								label="Candidate"
+								fullWidth
+								onChange={()=>{
+
+								}}
+								margin="normal"
+								variant="outlined"
+								style={{ margin: 3 }}
+							/>	
+
+						</Grid>
+						
+						<Grid item xs={6} style={{paddingLeft:3,paddingRight:3}}>
+							<TextField
+								id="outlined-name"
+								label="Referee"
+								fullWidth
+								onChange={()=>{
+
+								}}
+								margin="normal"
+								variant="outlined"
+								style={{ margin: 3 }}
+							/>	
+
+						</Grid>
+					</Grid>
+				</Grid>
+			
+			{/* third row ends */}
+
+			
+			{/* forth row */}
+			
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs={6} sm={6}>
+					<p style={{paddingLeft:6}}>Employment history and references - System score</p>
+				</Grid>
+				<Grid item xs={6} sm={6} style={{paddingLeft:6}}>
+				<Select
+					fullWidth
+					native
+					// value={state.age}
+					onChange={()=>{
+
+					}}
+					inputProps={{
+						id: 'outlined-age-native-simple',
+					}}
+				>
+						<option value="" />
+						<option value={10}>Good</option>
+						<option value={20}>Other</option>
+						<option value={30}>Other</option>
+				</Select>
+				</Grid>		
+
+										
+				</Grid>
+			</Grid>
+
+			{/* forth row ends */}
+			
+				
+
+			{/* comments section */}
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs={12} sm={12} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						fullWidth
+						label="Comments"
+						id="outlined-dense-multiline"
+						margin="dense"
+						variant="outlined"
+						multiline
+						rowsMax="4"
+					/>
+				</Grid>
+				
+											
+			</Grid>
+			
+			{/* comments section ends */}
+
+			{/* Addition information section */}
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs={12} sm={12} style={{paddingLeft:3,paddingRight:3}}>
+					<TextField
+						fullWidth
+						label="Additional information"
+						id="outlined-dense-multiline"
+						margin="dense"
+						variant="outlined"
+						multiline
+						rowsMax="4"
+					/>
+				</Grid>
+				
+											
+			</Grid>
+			
+			{/* Addition information ends */}
+
+			{/* save button section */}
+			
+			<Grid container spacing={3} style={{marginTop:5}}>
+				<Grid item xs={2} sm={2} style={{paddingLeft:3,paddingRight:3}}>
+					<Button
+						fullWidth
+						variant="contained"
+						color="primary"
+						onClick={()=>{
+
+						}}
+						>
+						Save
+					</Button>
+			
+				</Grid>								
+			</Grid>
+
+			{/* save button section ends */}
+
+
+
+			
+
 		</Paper>
 	);
 }
