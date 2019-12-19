9502a3752e64b367da161831a865755730dcb235
Ally Jr <allyjr@pop-os.localdomain>
2019-12-19 18:26:31 +0300

    identity check fixes, social media fixes, minor fixes

--- src/constants.js
+++ src/constants.js
@@ -1,29 +1,37 @@
 // @ts-check
 export const countries = [
-	"australia",
-	"tanzania",
-	"uganda",
-	"kenya",
-	"rwanda",
-	"burundi",
-	"china",
-	"united states of america",
-	"japan",
-	"south africa",
-	"yemen",
-	"india",
-	"zambia"
+    "australia",
+    "tanzania",
+    "uganda",
+    "kenya",
+    "rwanda",
+    "burundi",
+    "china",
+    "united states of america",
+    "japan",
+    "south africa",
+    "yemen",
+    "india",
+    "zambia"
 ].sort();
 
 export const caseTypes = [
-	"fraud",
-	"theft",
-	"espionage",
-	"insider trading",
-	"other",
-]
-
+    "fraud",
+    "theft",
+    "espionage",
+    "insider trading",
+    "other"
+];
 
 export const KYC_COMPANY_DB_NAME = "company-analysis";
-export const KYC_SHAREHOLDER_ANALYSIS_DB_NAME = "share-holder-analysis"
+export const KYC_SHAREHOLDER_ANALYSIS_DB_NAME = "share-holder-analysis";
 export const KYC_ADDITIONAL_INFORMATION_DB_NAME = "additional-information";
+
+export const identificationTypes = {
+    "national-identification": "National Identification (NIDA)",
+    passport: "Passport",
+    "voter-id": "Voters ID",
+    "birth-certificate": "Birth Certificate",
+    "drivers-license": "Drivers License",
+    "social-security": "Social Security Number"
+};
